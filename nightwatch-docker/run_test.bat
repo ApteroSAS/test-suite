a87dc99e64cb42f718a48942774ac29bcfc976b1
Pierre-Henri <pierreh37@gmail.com>
2021-09-05 13:46:54 +0200

    save nightwatch xml repports

--- nightwatch-docker/run_test.bat
+++ nightwatch-docker/run_test.bat
@@ -1,7 +1,11 @@
-
+rem cleaning
 docker-compose kill && docker-compose rm -vf
+
+rem start test
 docker-compose build --pull nightwatch
 docker-compose up -d web hub node-chrome node-firefox
 timeout /t 5
-docker-compose run --rm nightwatch
+docker-compose run nightwatch
+
+rem cleaning
 docker-compose kill && docker-compose rm -vf
--- nightwatch-docker/scripts/run_nightwatch.sh
+++ nightwatch-docker/scripts/run_nightwatch.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 if [ "x$DEBUG" == "x" ] ; then
+  pwd
   npm run nightwatch
+  #npm run nightwatch --reporter ./html-reporter.js
+  #nightwatch-html-reporter -d /tests/nightwatch/reports
 else
   npm run nightwatch-debug
 fi
